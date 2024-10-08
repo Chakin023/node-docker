ec0983690ec3607fd6723025b5db6ff643904fef
Chakin Pattanasombatsakul <chakin.pattanasombatsakul@gmail.com>
2024-10-08 22:05:08 +0700

    add mongoDB

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 
 const app = express()
 
+mongoose
+    .connect("mongodb://chakin023:mypassword@mongo:27017/?authSource=admin")
+    .then(() => console.log("successfully connect to mongo DB"))
+    .catch((e) => console.log(e))
+
 app.get("/", (req, res) => {
     res.send("<h2>HI THERE!!!</h2>")
 })
