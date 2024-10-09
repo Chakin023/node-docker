5ac14c4c56bfc261299e988f6bb16bb3fef25a77
Chakin Pattanasombatsakul <chakin.pattanasombatsakul@gmail.com>
2024-10-09 18:03:23 +0700

    add CRUD post application

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
+const { 
+    MONGO_USER, 
+    MONGO_PASSWORD, 
+    MONGO_IP, 
+    MONGO_PORT 
+} = require("./config/config");
 
-const app = express()
+const postRouter = require("./routes/postRoutes");
+
+const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
-mongoose
+const connectWithRetry = () => {
+    mongoose
     .connect(mongoURL)
     .then(() => console.log("successfully connect to mongo DB"))
-    .catch((e) => console.log(e))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    })
+}
+
+connectWithRetry();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("<h2>HI THERE!!!</h2>")
 })
+
+app.use("/api/v1/posts", postRouter)
 
 const port = process.env.PORT || 3000;
 
